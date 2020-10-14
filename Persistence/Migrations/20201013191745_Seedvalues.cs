0808fa86665d6945f992b651c70f64449cb3112b
cor <cor.slagmolen@gmail.com>
2020-10-14 15:16:32 +0200

    Initial commit

---
+++ Persistence/Migrations/20201013191745_Seedvalues.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class Seedvalues : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "values",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 1, "value001" });
+
+            migrationBuilder.InsertData(
+                table: "values",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 2, "value002" });
+
+            migrationBuilder.InsertData(
+                table: "values",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 3, "value003" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "values",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "values",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "values",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
